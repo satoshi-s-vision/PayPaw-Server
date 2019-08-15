125ee89a3b563623dff597e6e093de7352f8c6b9
Dalin Huang <dhuan023@gmail.com>
2019-08-14 22:47:50 -0400

    update port

--- paypaw_server.js
+++ paypaw_server.js
@@ -11,7 +11,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8888;
 
 // Auth
 const passport = require('passport');
